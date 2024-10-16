c06280a065e8142794e9c6f070ef7cbcffd5c19a
Azureit <d.f.18th@gmail.com>
2024-10-16 18:39:24 +0900

    c

--- src/app/motion/page.tsx
+++ src/app/motion/page.tsx
@@ -4,13 +4,13 @@ import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <motion.div
             initial={{ opacity: 0 }} // 初期状態
             animate={{ opacity: 1 }} // アニメーション後の状態
             transition={{ duration: 1 }} // アニメーションの持続時間
         >
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-red)">
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-[#FFA500])">
                 <a className="text-xl">
                     Motion
                 </a>
