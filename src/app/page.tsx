18c8d4adb3baeeb9d8abfdb9ab58d1f71ca75fa0
Azureit <d.f.18th@gmail.com>
2024-10-16 18:24:28 +0900

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
+import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
+    <motion.div
+      initial={{ opacity: 0 }} // 初期状態
+      animate={{ opacity: 1 }} // アニメーション後の状態
+      transition={{ duration: 1 }} // アニメーションの持続時間
+    >
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
@@ -97,5 +103,6 @@ export default function Home() {
         </a>
       </footer>
     </div>
+    </motion.div>
   );
 }
