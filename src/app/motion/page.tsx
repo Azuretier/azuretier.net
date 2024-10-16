e7d31257b3ac5a32708cb9660f87f4590379dc70
Azureit <d.f.18th@gmail.com>
2024-10-16 18:33:13 +0900

    c

--- src/app/motion/page.tsx
+++ src/app/motion/page.tsx
@@ -2,19 +2,20 @@
 
 import { motion } from 'framer-motion';
 
-const Page = () => {
+export default function Home() {
   return (
-    <motion.div
-      initial={{ opacity: 0 }} // 初期状態
-      animate={{ opacity: 1 }} // アニメーション後の状態
-      transition={{ duration: 1 }} // アニメーションの持続時間
-    >
-      <div className="p-8">
-        <h1>こんにちは！</h1>
-        <p>これはフェードインアニメーションを持つページです。</p>
-      </div>
-    </motion.div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <motion.div
+            initial={{ opacity: 0 }} // 初期状態
+            animate={{ opacity: 1 }} // アニメーション後の状態
+            transition={{ duration: 1 }} // アニメーションの持続時間
+        >
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-red)">
+                <a className="text-xl">
+                    Motion
+                </a>
+            </main>
+        </motion.div>
+    </div>
   );
-};
-
-export default Page;
+}
