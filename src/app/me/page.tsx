917ce1008a5f670dbd6537ff18e6fa4cb399bc11
Azureit <d.f.18th@gmail.com>
2024-10-18 00:51:19 +0900

    c

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-900">
         <motion.div
-            initial={{ opacity: 0 }} // 初期状態
-            animate={{ opacity: 1 }} // アニメーション後の状態
+            initial={{ opacity: 0, y: 50 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション後の状態
             transition={{ duration: 2 }} // アニメーションの持続時間
+            className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
         >
-            <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
                 <div className="shrink-0">
                     <Image className="size-24" src="/azure.webp" width={800} height={800} quality={75} alt="Azure Logo"/>
                 </div>
@@ -19,7 +19,6 @@ export default function Home() {
                     <div className="text-xl font-medium text-black">Azure</div>
                     <p className="text-slate-500">You have a new message!</p>
                 </div>
-            </div>
         </motion.div>
     </div>
   );
