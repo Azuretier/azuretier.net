1da0aac016f0bedbc6b5f80656cdf84dfaa85faa
Azureit <d.f.18th@gmail.com>
2024-10-18 04:25:26 +0900

    c

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-900">
         <motion.div
-            initial={{ opacity: 0, y: 10 }}
+            initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{
               duration: 0.5,      // アニメーションの時間
