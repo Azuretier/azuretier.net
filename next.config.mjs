2c106ad60f12e5de89cac70c45fd75261b063da8
Azureit <d.f.18th@gmail.com>
2024-10-18 01:44:48 +0900

    c

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['azuret.net'], // 必要に応じてドメインを追加
-      loader: 'akamai', // 画像の最適化設定
-      path: '', // 画像パスを指定
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
