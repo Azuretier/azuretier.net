0009ca1736210f84bb3f15f361dc2f398cc369e7
Azureit <d.f.18th@gmail.com>
2024-10-18 04:54:41 +0900

    c

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{
-              type: 'spring',
+              y: {type: 'spring'}
             }}
             className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
         >
