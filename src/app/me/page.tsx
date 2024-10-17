7a4167b994b30915c57c5526f7a43b7e637c7272
Azureit <d.f.18th@gmail.com>
2024-10-18 03:23:50 +0900

    c

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -7,15 +7,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-900">
         <motion.div
-            initial={{ opacity: 0, y: -100 }}
-            animate={{
-                opacity: 1,
-                y: [-30, 10, 3, 0], // 上にバウンスしてから少し下にバウンド
-            }}
-            transition={{
-                duration: 1.5, // アニメーションの持続時間
-                ease: 'easeOut', // イージング
-            }}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y:0, transition: { type: 'spring'} }}
             className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
         >
             <div className="shrink-0">
