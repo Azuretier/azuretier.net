357ca5558d78923fa74b7d68bdef13c4a303c461
Azureit <d.f.18th@gmail.com>
2024-10-18 04:44:39 +0900

    c

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,20 +1,21 @@
-import { FadeUpDiv, FadeUpStagger } from '@/components/animation';
 import Image from "next/image";
+import { FadeUpDiv, FadeUpStagger } from '@/components/animation';
+
 
 export default function Home() {
   return (
-    <FadeUpStagger>
-        <FadeUpDiv className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-900">
-        
-            <div className="shrink-0">
-                <Image className="size-24" src="/azure.webp" width={800} height={800} quality={75} alt="Azure Logo"/>
-            </div>
-            <div>
-                <div className="text-xl font-medium text-black">Azure</div>
-                <p className="text-slate-500">You have a new message!</p>
-            </div>
-        
-        </FadeUpDiv>
-    </FadeUpStagger>
+    <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-900">
+        <FadeUpStagger>
+            <FadeUpDiv className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+                <div className="shrink-0">
+                    <Image className="size-24" src="/azure.webp" width={800} height={800} quality={75} alt="Azure Logo"/>
+                </div>
+                <div>
+                    <div className="text-xl font-medium text-black">Azure</div>
+                    <p className="text-slate-500">You have a new message!</p>
+                </div>
+            </FadeUpDiv>
+        </FadeUpStagger>
+    </div>
   );
 }
