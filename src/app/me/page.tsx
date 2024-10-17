72711163f2d44077c5257d01f808e1f6a9fddd35
Azureit <d.f.18th@gmail.com>
2024-10-18 05:00:15 +0900

    c

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,16 +1,18 @@
 'use client'
 
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-900">
+        <AnimatePresence>
         <motion.div
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0 }}
             transition={{
-              y: {type: 'spring'}
+              type: 'spring'
             }}
             className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
         >
@@ -22,6 +24,7 @@ export default function Home() {
                 <p className="text-slate-500">You have a new message!</p>
             </div>
         </motion.div>
+        </AnimatePresence>
     </div>
   );
 }
