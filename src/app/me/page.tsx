1b54591c53c1365508fa46a01ca3a37b98f2328d
Azureit <d.f.18th@gmail.com>
2024-10-18 20:04:19 +0900

    c

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -8,30 +8,38 @@ export default function Home() {
     const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
-        setIsLoaded(true);
+        const handlePageLoad = () => {
+            setIsLoaded(true); // ロード完了時にアニメーションを開始
+        };
+
+        // windowのloadイベントを監視
+        window.addEventListener('load', handlePageLoad);
+
+        // クリーンアップ関数でイベントリスナーを削除
+        return () => {
+            window.removeEventListener('load', handlePageLoad);
+        };
     }, []); // 初回レンダリング時のみ実行される
 
     return (
         <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-900">
-            <AnimatePresence>
-                <motion.div
-                    layout
-                    initial={{ opacity: 0, y: 10}}
-                    animate={isLoaded ? { opacity: 1, y: 0 } : {}}
-                    transition={{
-                        type: 'spring'
-                    }}
-                    className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
-                >
-                    <div className="shrink-0">
-                        <Image className="size-24" src="/azure.webp" width={800} height={800} quality={75} alt="Azure Logo"/>
-                    </div>
-                    <div>
-                        <div className="text-xl font-medium text-black">Azure</div>
-                        <p className="text-slate-500">You have a new message!</p>
-                    </div>
-                </motion.div>
-            </AnimatePresence>
+            <motion.div
+                layout
+                initial={{ opacity: 0, y: 10}}
+                animate={isLoaded ? { opacity: 1, y: 0 } : {}}
+                transition={{
+                    type: 'spring'
+                }}
+                className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
+            >
+                <div className="shrink-0">
+                    <Image className="size-24" src="/azure.webp" width={800} height={800} quality={75} alt="Azure Logo"/>
+                </div>
+                <div>
+                    <div className="text-xl font-medium text-black">Azure</div>
+                    <p className="text-slate-500">You have a new message!</p>
+                </div>
+            </motion.div>
         </div>
     );
 }
