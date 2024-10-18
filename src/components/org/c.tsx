6f0ea893f5e033d6cbfd20902550c1bc9463e9c9
Azureit <d.f.18th@gmail.com>
2024-10-19 07:57:11 +0900

    c

--- src/components/org/c.tsx
+++ src/components/org/c.tsx
@@ -1,12 +1,15 @@
-const konyuuta = () => {
-    return (
-        // items-center = vertical
-        // justify-center = horizontal
+//componentのNaming convention守るおれカッケェって思ってそうって言われるためにkonyuuta -> KonYuuta
+//componentとfuncの違い: componentはuiを, funcは動作を提供
 
+const KonYuuta = () => {
+
+    // items-center = vertical
+    // justify-center = horizontal
+    return (
         <main className="flex items-center justify-center h-screen">
             <div>aiueo</div>
         </main>
-    );
+    )
 }
 
-export default konyuuta
+export default KonYuuta
