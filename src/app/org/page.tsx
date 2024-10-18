f4f7b3d1700ca4aa4d6f571d489406dfa2a0c648
Azureit <d.f.18th@gmail.com>
2024-10-19 07:29:23 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -1,10 +1,6 @@
-//import adhd from "@/components/org/c"
+import adhd from "@/components/org/c"
 //import {kawaikouki} from "@/components/org/c"
 
 export default function gatiiku() {
-    return (
-        <main>
-            <div>aiueo</div>
-        </main>
-    );
+    adhd
 }
--- src/components/org/c.tsx
+++ src/components/org/c.tsx
@@ -1,12 +1,8 @@
 const konyuuta = () => {
     return (
-        <div>This is returned by konyuuta function in c.tsx</div>
-    );
-}
-
-export const kawaikouki = () => {
-    return (
-        <div>This is div returned by kawaikouki function in c.tsx</div>
+        <main>
+            <div>aiueo</div>
+        </main>
     );
 }
 
