55151c171dcc0d6066a6e78f9625b337f3db73b0
Azureit <d.f.18th@gmail.com>
2024-10-18 21:52:06 +0900

    c

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
     return (
         <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-900">
             <motion.div
-                initial={{ opacity: 0, y: 100, scale: 0}}
+                initial={{ opacity: 0, y: 10, scale: 0}}
                 animate={isLoaded ? { opacity: 1, y: 0, scale: 1} : {}}
                 transition={{
                     y: {
