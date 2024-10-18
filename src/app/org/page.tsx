8baba435762470bb4a596e5bb464549f98126acb
Azureit <d.f.18th@gmail.com>
2024-10-19 08:03:26 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -2,5 +2,7 @@ import ADHD from "@/components/org/c"
 //import {kawaikouki} from "@/components/org/c"
 
 export default function gatiiku() {
-    return <ADHD />
+    return (
+        <ADHD />
+    )
 }
