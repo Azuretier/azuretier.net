349dff2c0d26d9a1a5baaf7bd6f548b78da9c22f
Azureit <d.f.18th@gmail.com>
2024-10-20 08:28:02 +0900

    c

--- src/app/810/page.tsx
+++ src/app/810/page.tsx
@@ -16,8 +16,8 @@ const Gatiiku = () => {
             <motion.div
                 key={count}
                 className="bg-white p-4 rounded-xl text-xl"
-                initial={{ opacity: 0, scale: 0 }}
-                animate={{ opacity: 1, scale: 1 }}
+                initial={{scale: 0 }}
+                animate={{scale: 1 }}
             >
                 1919
             </motion.div>
