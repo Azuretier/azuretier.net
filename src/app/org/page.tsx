511f36551520cd010a5285f3cfdfe564d403f947
Azureit <d.f.18th@gmail.com>
2024-10-20 06:06:58 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -5,11 +5,15 @@
 import {useEffect} from 'react'
 import {animate} from 'framer-motion/dom'
 
+const fadeUpElement = document.getElementById("fadeUpElement")
+
 const Gatiiku = () => {
 
     useEffect(() => {
         // ページが読み込まれた時に isLoaded を true に設定
-        animate("div", { y: [10, 0] }, { type: "spring" })
+        if (fadeUpElement !== null) {
+            animate(fadeUpElement, { y: [10, 0], opacity: [0, 1] }, { type: "spring" })
+        }
     }, []); // 空の依存配列で初回レンダリング時にのみ実行
 
     return (
