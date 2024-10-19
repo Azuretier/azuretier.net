7abe390a9441c6f5cc57486c507c79a84927250d
Azureit <d.f.18th@gmail.com>
2024-10-20 07:32:52 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -8,9 +8,9 @@ import {animate} from 'framer-motion/dom'
 
 const Gatiiku = () => {
     useEffect(() => {
-        const fadeUp = null
+        const fadeUp = document.getElementById("fadeUp")
         if (fadeUp != null) {
-            animate(fadeUp, {opacity: [0, 1]})
+            animate(fadeUp, {opacity: 1})
         }
     }, []); // 空の依存配列で初回レンダリング時にのみ実行
 
@@ -18,9 +18,8 @@ const Gatiiku = () => {
         <main className="flex items-center justify-center h-screen">    
             <motion.div 
                 className="bg-white p-4 rounded-xl text-xl text-black"
-                initial={{opacity: 0}}
-                //animate={{y: 0}}
-                whileInView={{ opacity: 1 }}
+                initial={{y:10, opacity: 0}}
+                animate={{y: 0}}
                 transition={{type: 'spring'}}
                 id="fadeUp"
             >gatiiku</motion.div>
