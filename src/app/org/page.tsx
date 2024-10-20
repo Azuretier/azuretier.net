10312f84cb32651041802f2fb8844f0bc135ff17
Azureit <d.f.18th@gmail.com>
2024-10-20 10:41:26 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -18,7 +18,7 @@ const Gatiiku = () => {
         <main className="flex items-center justify-center h-screen">    
             <motion.div 
                 className="bg-white p-4 rounded-xl text-xl text-black"
-                initial={{y: 30, scale:0, opacity: 0}}
+                initial={{y: 100, scale:0, opacity: 0}}
                 animate={{y: 0, scale: 1}}
                 transition={{type: 'spring'}}
                 id="fadeUp"
