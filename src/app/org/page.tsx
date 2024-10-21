c523bd6b8d6f46fd5cf0fd75f592cfb3fb30b62e
Azureit <d.f.18th@gmail.com>
2024-10-21 18:41:18 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -28,7 +28,7 @@ const Main = () => {
                 id="fadeUp"
             >
                 <section className="text-3xl font-black justify-self-center">あずれーと</section>
-                <div className="grid grid-flow-col items-center justify-start gap-1">
+                <div className="grid grid-flow-col items-center justify-start gap-2">
                     <FaBirthdayCake/>
                     <p>200X/2/18</p>
                 </div>
