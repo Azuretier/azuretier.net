222472a9aff9183067eb8df4db95cfdee53a5e73
Azureit <d.f.18th@gmail.com>
2024-10-22 22:50:03 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -22,31 +22,28 @@ const Main = () => {
     return (
         <main className="grid grid-cols-12 grid-rows-3 grid-flow-row gap-4 items-center justify-center h-screen">   
             <motion.div 
-                className="grid gap-6 p-4 rounded-xl text-xl text-white row-start-2 col-start-2 col-span-3"
+                className="grid gap-4 p-4 rounded-xl text-base font-black text-white row-start-2 col-start-2 col-span-3"
                 initial={{y: 20, opacity: 0}}
                 animate={{y: 0}}
                 transition={{type: 'spring'}}
                 id="fadeUp"
             >
-                <section className="grid justify-center gap-6 text-base font-black">
-                    <Image src="/azure.png" alt="avatar" width={200} height={200}/>
-                    <div className="grid row-start-2">
-                        <p className="text-3xl">Azuret</p>
-                        <p className="text-subtext">あずれーと</p>
-                        <p className="font-normal text-subtext text-lg">世界は自分で創る</p>
+                <Image src="/azure.png" alt="avatar" width={200} height={200}/>
+                <div className="grid row-start-2">
+                    <p className="text-3xl">Azuret</p>
+                    <p className="text-subtext">あずれーと</p>
+                    <p className="font-normal text-subtext text-lg">世界は自分で創る</p>
+                </div>
+                <div className="grid grid-flow-row gap-1 text-subtext">
+                    <div className="grid grid-flow-col gap-2 justify-start">
+                        <FaBirthdayCake/>
+                        <p>200X/2/18</p>    
                     </div>
-                    <div className="grid grid-flow-row gap-1 text-subtext">
-                        <div className="grid grid-flow-col gap-2 justify-start">
-                            <FaBirthdayCake/>
-                            <p>200X/2/18</p>    
-                        </div>
-                        <div className="grid grid-flow-col gap-2  justify-start">
-                            <FaAngleDoubleUp/>
-                            <p>Get experience to level up</p>
-                        </div>
+                    <div className="grid grid-flow-col gap-2  justify-start">
+                        <FaAngleDoubleUp/>
+                        <p>Get experience to level up</p>
                     </div>
-                </section>
-                
+                </div>     
             </motion.div>
         </main>
     )
