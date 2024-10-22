ec31eb2f5fd454e7a9f72053ed349cc611b46eb4
Azureit <d.f.18th@gmail.com>
2024-10-22 22:44:37 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -28,23 +28,23 @@ const Main = () => {
                 transition={{type: 'spring'}}
                 id="fadeUp"
             >
-                <section className="grid justify-center gap-1 text-base font-black">
+                <section className="grid justify-center gap-4 text-base font-black">
                     <Image src="/azure.png" alt="avatar" width={200} height={200}/>
                     <div className="grid row-start-2">
                         <p className="text-3xl">Azuret</p>
                         <p className="text-subtext">あずれーと</p>
                         <p className="font-normal text-subtext text-lg">世界は自分で創る</p>
                     </div>
-                <div className="grid grid-flow-row gap-1 text-subtext">
-                    <div className="grid grid-flow-col gap-2 justify-start">
-                        <FaBirthdayCake/>
-                        <p>200X/2/18</p>    
+                    <div className="grid grid-flow-row gap-1 text-subtext">
+                        <div className="grid grid-flow-col gap-2 justify-start">
+                            <FaBirthdayCake/>
+                            <p>200X/2/18</p>    
+                        </div>
+                        <div className="grid grid-flow-col gap-2  justify-start">
+                            <FaAngleDoubleUp/>
+                            <p>Get experience to level up</p>
+                        </div>
                     </div>
-                    <div className="grid grid-flow-col gap-2  justify-start">
-                        <FaAngleDoubleUp/>
-                        <p>Get experience to level up</p>
-                    </div>
-                </div>
                 </section>
                 
             </motion.div>
