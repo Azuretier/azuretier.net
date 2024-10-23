ee979085eee805c605575baecd2757f86c7c21aa
Azureit <d.f.18th@gmail.com>
2024-10-23 20:41:49 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -23,7 +23,7 @@ const Main = () => {
     return (
         <main className="grid grid-cols-12 grid-rows-6 grid-flow-row items-center justify-center h-screen">   
             <motion.div 
-                className="grid gap-4 p-4 rounded-xl text-base font-black text-white row-start-3 col-start-2 col-span-3"
+                className="grid gap-4 p-4 rounded-xl text-base font-black text-white row-start-3 col-start-2 row-span-2 col-span-3"
                 initial={{y: 20, opacity: 0}}
                 animate={{y: 0}}
                 transition={{type: 'spring'}}
