c432acd6c056549d3b736f5ae94e67574d532aab
Azureit <d.f.18th@gmail.com>
2025-07-16 22:00:24 +0900

    c

--- src/app/3/page.tsx
+++ src/app/3/page.tsx
@@ -64,28 +64,28 @@ const Main = () => {
       {/* Main Content */}
       <main className="grid grid-cols-12 grid-rows-6 grid-flow-row items-center justify-center h-screen bg-black">
         <motion.div
-          className="grid gap-4 p-4 rounded-xl text-base font-black text-white row-start-3 col-start-2 row-span-2 col-span-5"
+          className="grid gap-4 p-4 rounded-xl text-base font-black text-white grid-cols-1 grid-flow-row row-start-3 col-start-2 row-span-2 col-span-5"
           initial={{ y: 20, opacity: 0 }}
           animate={{ y: 0 }}
           transition={{ type: "spring" }}
           id="fadeUp"
         >
           <Image src="/azure.png" alt="avatar" width={200} height={200} />
-          <div className="grid row-start-2 col-span-3">
+          <div className="grid col-span-3">
             <p className="text-3xl">Azuret</p>
             <p className="text-subtext">あずれーと</p>
             <p className="font-normal text-subtext text-lg">I make my world myself</p>
           </div>
-          <div className="grid gap-1 text-subtext row-start-3 grid-rows-3">
-            <div className="grid justify-start gap-2 content-center grid-flow-col row-start-1">
+          <div className="grid gap-1 text-subtext grid-flow-row grid-rows-3">
+            <div className="grid justify-start gap-2 content-center grid-flow-col">
               <FaBirthdayCake />
               <p>200X/2/18</p>
             </div>
-            <div className="grid justify-start gap-2 content-center grid-flow-col row-start-2">
+            <div className="grid justify-start gap-2 content-center grid-flow-col">
               <FaUserGraduate />
               <p>Student</p>
             </div>
-            <div className="grid justify-start gap-2 content-center grid-flow-col row-start-3">
+            <div className="grid justify-start gap-2 content-center grid-flow-col">
               <FaPaperPlane />
               <div className="grid grid-rows-1 grid-cols-2">
                 <span id="rotating-text">{displayedText}</span>
