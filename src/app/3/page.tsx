dea66e361c0f7dd3f1af0d62d7c7cb04546ebf31
Azureit <d.f.18th@gmail.com>
2025-07-18 02:30:11 +0900

    c

--- src/app/3/page.tsx
+++ src/app/3/page.tsx
@@ -99,7 +99,7 @@ const Main = () => {
               </div>
             </div>
           </motion.div>
-          <div className="grid gap-4 p-4 rounded-xl text-base font-black text-white grid-rows-6 grid-cols-6 grid-flow-col row-start-3 col-start-6 row-span-2 col-span-3 justify-around">
+          <div className="grid gap-4 p-4 rounded-xl text-base font-black text-white grid-rows-6 grid-cols-6 grid-flow-col row-start-3 col-start-6 row-span-2 col-span-3">
             <motion.a href="https://x.com/09xgg" className="fade-up grid place-items-center gap-3 text-white border-2 border-black outline outline-2 outline-gray-400 outline-offset-0 rounded-lg row-span-2 col-span-2">
               <FaXTwitter size={24} />
             </motion.a>
