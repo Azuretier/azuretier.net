346074d5f2695a71a65844f838f54bea0a7b35e1
Azureit <d.f.18th@gmail.com>
2025-07-18 22:19:50 +0900

    changed font-color to default

--- src/app/3/page.tsx
+++ src/app/3/page.tsx
@@ -73,11 +73,11 @@ const Main = () => {
       {/* Main Content */}
       <main className="grid grid-cols-12 grid-rows-7 grid-flow-row items-center justify-center h-screen bg-white">
           <motion.div
-            className="fade-up grid gap-4 p-4 rounded-xl text-base text-white grid-cols-1 grid-flow-row row-start-3 col-start-2 row-span-3 col-span-4"
+            className="fade-up grid gap-4 p-4 rounded-xl text-base grid-cols-1 grid-flow-row row-start-3 col-start-2 row-span-3 col-span-4"
           >
             <Image src="/azure.png" alt="avatar" width={200} height={200} />
             <div className="grid col-span-3">
-              <p className="text-3xl font-black">Azuret</p>
+              <p className="text-3xl font-black">Azure</p>
               <p className="text-subtext font-black">あずれーと</p>
               <p className="font-normal text-subtext text-lg">I make my world myself</p>
             </div>
