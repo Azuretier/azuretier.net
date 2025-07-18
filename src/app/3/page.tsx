8c12d27782eb9fd35ee095bec1d5d0458d6d9c0c
Azureit <d.f.18th@gmail.com>
2025-07-18 17:32:51 +0900

    removed needless shadows

--- src/app/3/page.tsx
+++ src/app/3/page.tsx
@@ -101,17 +101,17 @@ const Main = () => {
           </motion.div>
           <div className="grid p-4 rounded-xl text-base font-black text-white grid-rows-7 grid-cols-9 grid-flow-row row-start-3 col-start-6 row-span-3 col-span-6 h-full">
             <motion.a href="https://x.com/09xgg" className="fade-up grid place-items-center gap-3 text-white border shadow transition-colors hover:border-gray-400 rounded-lg row-span-3 col-span-4">
-              <div className="flex h-10 w-10 items-center justify-center rounded-xl text-white bg-black shadow">
+              <div className="flex h-10 w-10 items-center justify-center rounded-xl text-white bg-black">
                 <FaXTwitter size={24} />
               </div>
             </motion.a>
             <motion.a href="https://github.com/Azuretier" className="fade-up grid place-items-center gap-3 col-start-6 text-white border shadow transition-colors hover:border-gray-400 rounded-lg row-span-3 col-span-4">
-              <div className="flex h-10 w-10 items-center justify-center rounded-xl text-white bg-black shadow">
+              <div className="flex h-10 w-10 items-center justify-center rounded-xl text-white bg-black">
                 <FaGithub size={24} />
               </div>
             </motion.a>
             <motion.a href="https://discord.gg/XkwSarHyQm" className="fade-up grid place-items-center gap-3 row-start-5 border shadow transition-colors hover:border-[rgb(88,101,242)] rounded-lg row-span-3 col-span-4">
-              <div className="flex h-10 w-10 items-center justify-center rounded-xl text-white bg-[rgb(88,101,242)] shadow">
+              <div className="flex h-10 w-10 items-center justify-center rounded-xl text-white bg-[rgb(88,101,242)]">
                 <FaDiscord size={24} />
               </div>
             </motion.a>
