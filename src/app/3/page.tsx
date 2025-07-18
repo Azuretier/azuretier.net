51963a906f349309f88bf3b3d2a925acaa114e75
Azureit <d.f.18th@gmail.com>
2025-07-18 16:31:56 +0900

    adjusted pos

--- src/app/3/page.tsx
+++ src/app/3/page.tsx
@@ -103,13 +103,13 @@ const Main = () => {
             <motion.a href="https://x.com/09xgg" className="fade-up grid place-items-center gap-3 text-white border-2 border-black outline outline-2 outline-gray-400 outline-offset-0 rounded-lg row-span-3 col-span-4">
               <FaXTwitter size={24} />
             </motion.a>
-            <motion.a href="https://github.com/Azuretier" className="fade-up grid place-items-center gap-3 col-start-5 text-white border-2 border-black outline outline-2 outline-gray-400 outline-offset-0 rounded-lg row-span-3 col-span-4">
+            <motion.a href="https://github.com/Azuretier" className="fade-up grid place-items-center gap-3 col-start-6 text-white border-2 border-black outline outline-2 outline-gray-400 outline-offset-0 rounded-lg row-span-3 col-span-4">
               <FaGithub size={24} />
             </motion.a>
-            <motion.a href="https://discord.gg/XkwSarHyQm" className="fade-up grid place-items-center gap-3 row-start-4 text-indigo-400 border-2 border-black outline outline-2 outline-indigo-400 outline-offset-0 rounded-lg row-span-3 col-span-4">
+            <motion.a href="https://discord.gg/XkwSarHyQm" className="fade-up grid place-items-center gap-3 row-start-5 text-indigo-400 border-2 border-black outline outline-2 outline-indigo-400 outline-offset-0 rounded-lg row-span-3 col-span-4">
               <FaDiscord size={24} />
             </motion.a>
-            <motion.a href="https://youtube.com/@Azuret" className="fade-up grid place-items-center gap-3 row-start-4 col-start-5 text-red-500 border-2 border-black outline outline-2 outline-red-500 outline-offset-0 rounded-lg row-span-3 col-span-4">
+            <motion.a href="https://youtube.com/@Azuret" className="fade-up grid place-items-center gap-3 row-start-5 col-start-6 text-red-500 border-2 border-black outline outline-2 outline-red-500 outline-offset-0 rounded-lg row-span-3 col-span-4">
               <FaYoutube size={24} />
             </motion.a>
           </div>
