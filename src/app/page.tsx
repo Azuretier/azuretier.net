f29a0084ab41dd55c5d9e34c008aec135ee49861
Azureit <d.f.18th@gmail.com>
2025-08-15 17:39:15 +0900

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,8 +72,9 @@ const Main = () => {
 
   return (
     <>
-      {/* Main Content */}
-      <main id="container" className="bg-[url('/mnse_1.png')] bg-cover bg-center bg-no-repeat backdrop-blur grid grid-cols-12 grid-rows-7 grid-flow-row items-center justify-center h-screen">
+      {/* Main Content */}\
+      <div id="container"></div>
+      <main className="bg-[url('/mnse_1.png')] bg-cover bg-center bg-no-repeat backdrop-blur grid grid-cols-12 grid-rows-7 grid-flow-row items-center justify-center h-screen">
           <motion.div className="fade-up grid bg-black/70 p-6 rounded-lg items-center grid-cols-1 grid-rows-4 row-start-3 col-start-2 row-span-3 col-span-3 h-full">
             <div className="flex h-50 w-50 items-center justify-start row-span-2">
               <Image src="/azure.png" alt="avatar" width={200} height={200} />
