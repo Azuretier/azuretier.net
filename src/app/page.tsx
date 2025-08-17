574e07cdd6d3ca735092e8fea9a28529bbab6119
Azureit <d.f.18th@gmail.com>
2025-08-17 21:59:37 +0900

    fix font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,7 +145,7 @@ const Main = () => {
           <p className="text-2xl font-bold">Azure</p>
           <p className="text-sm text-gray-500">He/Him</p>
         </div>
-        <div className="grid row-span-2 text-lg font-sanserif row-start-4">
+        <div className="grid row-span-2 text-lg font-bold row-start-4">
           <div className="grid justify-start gap-2 grid-flow-col items-center">
             <FaBirthdayCake />
             <p>200X/2/18</p>
@@ -156,7 +156,7 @@ const Main = () => {
           </div>
           <div className="grid justify-start gap-2 grid-flow-col items-center">
             <FaPaperPlane />
-            <div className="grid font-Arial grid-rows-1 grid-flow-col justify-start">
+            <div className="grid grid-rows-1 grid-flow-col justify-start">
               <span>{displayedText}</span>
               <span className="blinking">|</span>
             </div>
