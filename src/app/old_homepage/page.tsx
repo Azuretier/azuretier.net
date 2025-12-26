e187f5cc2c2848d33434c95cb20a4adff06b0cd2
Azureit <d.f.18th@gmail.com>
2025-12-26 14:16:37 +0900

    adjust 4

--- src/app/old_homepage/page.tsx
+++ src/app/old_homepage/page.tsx
@@ -161,7 +161,7 @@ const Main = () => {
         </div>
       </motion.div>
       <div className="grid rounded-xl grid-rows-7 grid-cols-9 row-start-3 col-start-6 row-span-3 col-span-6 h-full">
-        <GradientWrapper href="https://www.instagram.com/rrrrrrrrrrvq/" target="_blank" rel="noopener noreferrer" className="fade-up grid backdrop-blur-xl bg-[var(--widget-bg)] gap-3 p-6 border border-[var(--widget-border)] shadow-md transition-colors hover:border-[radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%)] rounded-lg row-span-3 col-span-4">
+        <GradientWrapper href="https://www.instagram.com/rrrrrrrrrrvq/" target="_blank" rel="noopener noreferrer" className="fade-up grid bg-[var(--widget-bg)] gap-3 p-6 border border-[var(--widget-border)] shadow-md transition-colors hover:border-[radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%)] rounded-lg row-span-3 col-span-4">
           <div className="flex h-20 w-20 items-center justify-center rounded-2xl text-white bg-instagram-gradient drop-shadow-md">
             <FaInstagram size={48} />
           </div>
